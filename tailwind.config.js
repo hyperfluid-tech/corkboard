d23902f4a20fa63c2d3fb587b27d699f85a43a03
Gil Nobrega <82336674+gilnobrega@users.noreply.github.com>
2026-06-04 23:37:06 +0100

    change css order

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./templates/*.html",
     "./templates/**/*.html",
-    "./templates/**/*.js",
+    "./templates/script/*.js",
     "./src/**/*.rs",
   ],
   darkMode: "class",
